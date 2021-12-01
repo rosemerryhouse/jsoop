60a72da65c608268564e59188710420531263bbc
Rose <rosehousetea@gmail.com>
2021-12-01 17:04:21 +0800

    update exercise c9

--- exercise/7-28.js
+++ exercise/7-28.js
@@ -1,19 +1,22 @@
 //es5
-var dst  = {quux: 0};
-var src1 = {foo: 1, bar: 2};
-var src2 = {foo: 3, baz: 4};
-Object.keys(src1).forEach(function(k) {
-    dst[k] = src1[k];
-});
-Object.keys(src2).forEach(function(k) {
-    dst[k] = src2[k];
-});
-console.log(dst);
-//es6
-{
-    let dst  = {quux: 0}
-    let src1 = {foo: 1, bar: 2}
-    let src2 = {foo: 3, baz: 4}
+var dst = { quux: 0 };
+var src1 = { foo: 1, bar: 2 };
+var src2 = { foo: 3, baz: 4 };
 
-    console.log(dst);
+// 淺層拷貝才可以用
+// Object.keys(src1).forEach(function (k) {
+//   dst[k] = src1[k];
+// });
+
+// Object.keys(src2).forEach(function (k) {
+//   dst[k] = src2[k];
+// });
+// console.log(dst);
+//es6 (只針對淺層拷貝，深層要用 deep 或用 loadash clone deep)
+{
+  let dst = { quux: 0 };
+  let src1 = { foo: 1, bar: 2 };
+  let src2 = { foo: 3, baz: 4 };
+  Object.assign(dst, src1, src2);
+  console.log(dst);
 }
--- exercise/7-29.js
+++ exercise/7-29.js
@@ -1,5 +1,6 @@
-//es5
+//es5 會產生一個新的 陣列裏
 console.log([1, 3, 4, 2].filter(function(x) {
     return x > 3;
 })[0]);
 //es6
+console.log([1, 3, 4, 2].find(x => x > 3));
